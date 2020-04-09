97009ec961830c46a6b920136835274ebbed9681
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-04-09 01:40:00 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,17 @@
+class Post 
+
+attr_accessor :title 
+
+def initialize(title)
+  @title = title
+end 
+
+ def add_post_by_title
+   title = self.new(name)
+ end    
+
+
+
+
+
+end 
---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song 
+  
+  attr_accessor  :name
+  attr_reader :artist 
+  
+  @@all = [ ]
+
+ def initialize(name, artist = nil)
+   @name = name 
+   @artist = artist 
+   @@all << self 
+  end 
+  
+   def artist=(a)
+     @artist = a 
+     @artist.songs << self 
+   end 
+     
+     
+  def aritst_name 
+    if self.artist 
+    else 
+      nil 
+    end
+  end 
+  
+  
+  
+  def self.all
+    @@all 
+  end 
+  
+end 
